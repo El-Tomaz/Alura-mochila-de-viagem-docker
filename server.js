d1b457142cb8ead69c4e17a2d31a8e0d364a4965
Gabriel_Tomaz <tomazlemosgarcia@gmail.com>
2023-08-13 19:16:55 -0300

    consertando repo anterior

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express()
 const port = 3000
 const fs = require('fs/promises')
 
-app.use(express.static(`${__dirname}client`))
+app.use(express.static(`${__dirname}/client`))
 
  app.get('/', async (req, res) => {
-  const index = await fs.readFile(`${__dirname}client/index.html`,{enconding:'utf8'})
+  const index = await fs.readFile(`${__dirname}/client/index.html`,{enconding:'utf8'})
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(index)
   res.end
